e6a03b15df3f7a07a187a0c831c15977886aa88f
Lauren Gonzalez <lag117@gmail.com>
2015-10-27 12:08:34 -0400

    #This code takes the house elves data set and splits them into two #groups, large and small, based on ear size and adds this column to the #dataset. It then calculates GC content of the provided DNA sequences #and adds this percentage as a column to the dataset. It then writes #a new csv file with the ID#, ear size class, and GC content columns.

---
+++ houseelf-analysis.R
@@ -0,0 +1,57 @@
+#This code takes the house elves data set and splits them into two
+#groups, large and small, based on ear size and adds this column to the 
+#dataset. It then calculates GC content of the provided DNA sequences
+#and adds this percentage as a column to the dataset. It then writes
+#a new csv file with the ID#, ear size class, and GC content columns.
+library(dplyr)
+#import data
+data <- read.csv("http://www.datacarpentry.org/semester-biology/data/houseelf_earlength_dna_data.csv")
+#function that splits things into two size classes, large and small,
+#based on ear length and a threshold that the user can set
+get_size_class <- function(earlength, threshold){
+  if (earlength > threshold){
+    size_class = "large"
+  }
+  else {
+    size_class = "small"
+  }
+  return(size_class)
+}
+
+#function that uses the get_size_class function to separate
+#all data into small and large size classes and adds this column
+#to the existing data
+add_size_classes <- function(df){
+  #add size class data to a dataframe. input dataframe with weight column containing size info
+  data_size_class <-
+    df %>%
+    na.omit() %>%
+    rowwise() %>%
+    mutate(size_class = get_size_class(earlength, 10))
+  return(data_size_class)
+}
+
+#function that calculates GC content
+GCcontent <- function(sequence){
+  Gs <- str_count(sequence, 'G')
+  Cs <- str_count(sequence, 'C')
+  gc_content <- (Gs + Cs) / str_length(sequence) * 100
+  return(gc_content)
+}
+
+#function that uses the GCcontent function to calculate GC content
+#for each DNA sequence and adds a column with all percentages 
+#to the existing dataset
+add_GC <- function(df){
+  gccontentadd <-
+    df %>%
+    na.omit() %>%
+    rowwise() %>%
+    mutate(gc_content = GCcontent(dnaseq))
+  return(gccontentadd)
+}
+#takes all previous functions and adds appropriate columns
+#then writes a csv with the columns: id, size class, and gc content
+data_sizes <- add_size_classes(data)
+data_sizes_and_gc <- add_GC(data_sizes)
+write.csv(data_sizes_and_gc[,c("id","size_class","gc_content")], file="size_and_gc.csv")
