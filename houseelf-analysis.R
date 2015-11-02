26d8bddda2dea35aec237cba60e17b7e54d5a3f8
Lauren Gonzalez <laurenagonzalez@ufl.edu>
2015-11-02 17:49:52 -0500

    added str_to_upper in the gc content calculation function so that it will change any lower case sequences to upper so it can read any files regardless of capitalization

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -38,6 +38,7 @@ add_size_classes <- function(df){
 
 #function that calculates GC content
 GCcontent <- function(sequence){
+  sequence <- str_to_upper(sequence)
   Gs <- str_count(sequence, 'G')
   Cs <- str_count(sequence, 'C')
   gc_content <- (Gs + Cs) / str_length(sequence) * 100
