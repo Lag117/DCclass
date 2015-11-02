90e295278b78996268f098746ea5c048f274388a
Lauren Gonzalez <laurenagonzalez@ufl.edu>
2015-11-02 17:43:19 -0500

    changed file name to add _1 to distinguish because this will be first of many files

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -9,7 +9,7 @@ library(dplyr)
 #import data
 #from web: data <- read.csv("http://www.datacarpentry.org/semester-biology/data/houseelf_earlength_dna_data.csv")
 #from data subdirectory for problem 3
-data <- read.csv("~/myclass/data/houseelf_earlength_dna_data.csv")
+data <- read.csv("~/myclass/data/houseelf_earlength_dna_data_1.csv")
 
 #function that splits things into two size classes, large and small,
 #based on ear length and a threshold that the user can set
